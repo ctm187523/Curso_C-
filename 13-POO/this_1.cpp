b2802b09f88e562978b18af1ab715dd1589332fd
ctm <clemente1875@gmail.com>
2025-03-08 17:48:22 +0100

    Inicio del uso del This en C++

---
+++ 13-POO/this_1.cpp
@@ -0,0 +1,18 @@
+/*
+
+- ¿Qué es "this"?
+  -Palabra reservada en C++, podemos traducir mentalmento por "este mismo"
+  - En C++ this ers un puntero que contiene la dirección de memoria del objeto actual
+  - Solo puede usarse en el ámbito de la clase
+  - Cuando se accede a campos de clase, implícitamnete se accede utilizando "this"
+
+  -¿Para qué se utiliza?
+   - Para acceder a propiedades y métodos.
+   - Evita errores de ambigüedad de nombres.
+   - Para comparar si dos objetos son el mismo objeto
+   - Para desreferenciar(*this) y así obtener el objeto actual.Muy utilizado para llamadas encadenadas.
+
+
+*/
+
+//SIGUE EN MINUTO 5:12
